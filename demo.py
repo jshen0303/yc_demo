01cfc0519acaa340db61716fb05ae7727a10b8ff
Jeff Shen <jshen0303@gmail.com>
2025-10-08 20:26:48 -0700

    feature

--- demo.py
+++ demo.py
@@ -1,20 +1,19 @@
-#!/usr/bin/env python3
+
 import os, sys
 
 def main():
-    # Check for exported secret
-    secret = os.environ.get("function_param")
-    if not secret:
+   
+    context = os.environ.get("function_param")
+    if not context:
         print("Missing exported function_param!")
         return
 
-    # Require 2 or 3 positional parameters
+    # 2 or 3 params
     args = sys.argv[1:]
     if len(args) not in (2, 3):
         print("missing function parameters")
         return
 
-    # (Do whatever you want here; we just confirm success)
     print("Success!")
 
 if __name__ == "__main__":
